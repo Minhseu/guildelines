931384038c69c608e535b9748186d357b42a93df
Minhseu <minhseuhin@gmail.com>
2025-05-15 18:07:49 +0700

    add loading skeleton

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -7,24 +7,25 @@ interface CardProps {
 }
 
 export default function Card({ imageSrc, title, description }: CardProps) {
-    const [loading, setLoading] = useState<boolean>(false);
-    useEffect(() => {
-        if (imageSrc != '') {
-            setLoading(true);
-        }
-    }, [imageSrc]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     return (
         <div className="w-[500px] h-[400px] max-md:h-[400px]  border-2 border-gray-200 shadow-md rounded-lg overflow-hidden transform transition duration-300 hover:scale-105 flex flex-col">
             <div className="h-[80%] w-full relative bg-gray-100">
+                {/* Skeleton while loading */}
+                {loading && (
+                    <div className="absolute inset-0 wave-skeleton z-10"></div>
+                )}
+
+                {/* Image (always rendered) */}
                 <Image
+                    onLoad={() => setLoading(false)}
                     src={imageSrc}
                     alt={title}
                     width={700}
                     height={440}
-                    onLoad={() => setLoading(true)}
                     className={`w-full h-full object-contain transition duration-500 ${
-                        loading ? 'blur-0' : 'blur-md'
+                        loading ? 'blur-md scale-105' : 'blur-0 scale-100'
                     }`}
                 />
             </div>
